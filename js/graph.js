2586bc7a775327384305dbc2b84ab68d6e0d1ce2
John Chambers-Malewig <johncm@rcn.com>
2020-07-02 06:06:49 +0000

    Added Transition Factory

--- js/graph.js
+++ js/graph.js
@@ -1,5 +1,17 @@
+var millisecondDuration=750
+var millisecondDelay=40
+
+function transitionFactory() {
+    return d3.transition("main")
+        .ease(d3.easeLinear)
+        .delay(millisecondDelay)
+        .duration(millisecondDuration);
+}
+
 var graphviz = d3.select("#graph").graphviz()
     .engine()
+    .transition(transitionFactory)
+    .tweenShapes(false)
     .on('initEnd', function () {
         render(d3.select("#graph").attr("data-graph"), d3.select("#graph").attr("data-engine"));
     });
